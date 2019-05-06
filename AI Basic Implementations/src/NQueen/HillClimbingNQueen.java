19a91ba12f499c4a954edf26fb70026a6f7bcf52
Shamin Asfaq <shamin.asfaq@gmail.com>
2019-05-06 16:39:36 +0600

    Update HillClimbingNQueen.java

--- AI Basic Implementations/src/NQueen/HillClimbingNQueen.java
+++ AI Basic Implementations/src/NQueen/HillClimbingNQueen.java
@@ -22,71 +22,22 @@ public class HillClimbingNQueen {
     public void SolveNQueen() {
         int globalDimension = this.numberOfQueens;
 
-//         Time delay to watch the process output
-//        int conspiracy = 1000000000;
-//        int theory = 1000000000;
-//        int intact = 1000000000;
-//        int together = 1000000000;
-//        while (conspiracy >= 0) {
-//            while (theory >= 0) {
-//                while(intact >= 0){
-//                    while(together >= 0){
-//                        --together;
-//                    }
-//                    --intact;
-//                }
-//                --theory;
-//            }
-//            --conspiracy;
-//        }
-
 //        Timer starts
         long start = System.currentTimeMillis();
 
         int[][] board = new int[globalDimension][globalDimension];
         int[] flag = new int[(globalDimension * globalDimension)];
 
-//        System.out.println("--------------------------------------");
-//        System.out.println("EMPTY BOARD");
-//        System.out.println("--------------------------------------");
-//        for (int[] value : board) {
-//            for (int denim : value) {
-//                System.out.print(denim + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("--------------------------------------");
-
-//        for (int r = 0; r < globalDimension; r++) {
-//            for (int col = 0; col < globalDimension; col++) {
-//                board[r][col] = 0;
-//            }
-//        }
-
         for (int r = 0; r < globalDimension; r++) {
             for (int col = 0; col < globalDimension; col++) {
                 int random = (int) Math.floor(Math.random() * (globalDimension - 1));
-//                        System.out.println(" ////////////////// " + random);
                 board[random][r] = 1;
                 break;
             }
         }
 
-
-//        System.out.println("--------------------------------------");
-//        System.out.println("First Random BOARD");
-//        System.out.println("--------------------------------------");
-//        for (int[] value : board) {
-//            for (int denim : value) {
-//                System.out.print(denim + " ");
-//            }
-//            System.out.println();
-//        }
-//        System.out.println("--------------------------------------");
-
         int totalIterations = 0;
 
-//        int heuristic = findHeuristic(board);
         int[][] heuristicTable = new int[globalDimension][globalDimension];
         int lowestHeuristic = 100000;
 
@@ -112,30 +63,7 @@ public class HillClimbingNQueen {
                     newBoard[i][j] = bestSolution[i][j];
                 }
             }
-
-//            if (lowestHeuristic == 0) {
-//                System.out.println("Current ITERATION " + totalIterations);
-//                System.out.println("Lowest heuristic found: " + lowestHeuristic);
-//                System.out.println("--------------------------------------");
-//                System.out.println("BEST solution so far:");
-//                System.out.println("--------------------------------------");
-//                for (int[] value : bestSolution) {
-//                    for (int denim : value) {
-//                        System.out.print(denim + " ");
-//                    }
-//                    System.out.println();
-//                }
-//                System.out.println("--------------------------------------");
-//                System.out.println("--------------------------------------");
-//                for (int[] value : heuristicTable) {
-//                    for (int denim : value) {
-//                        System.out.print(denim + " ");
-//                    }
-//                    System.out.println();
-//                }
-//                System.out.println("--------------------------------------");
-//            }
-
+            
             for (int row = 0; row < globalDimension; row++) {
                 for (int col = 0; col < globalDimension; col++) {
                     if (newBoard[row][col] == 1) {
@@ -148,8 +76,6 @@ public class HillClimbingNQueen {
 
                             if (tempHeuristic < lowestHeuristic) {
                                 lowestHeuristic = tempHeuristic;
-//                                bestSolution[temp][col] = 1;
-//                                bestSolution[row][col] = 0;
                                 for (int i = 0; i < globalDimension; i++) {
                                     for (int j = 0; j < globalDimension; j++) {
                                         bestSolution[i][j] = newBoard[i][j];
@@ -157,43 +83,19 @@ public class HillClimbingNQueen {
                                 }
 
                                 heuristicTable[row][col] = tempHeuristic;
-//                                System.out.println(" ----> Lowest heuristic found: " + lowestHeuristic);
-//                                System.out.println("--------------------------------------");
-//                                System.out.println("----------------(BOARD)---------------");
-//                                System.out.println("--------------------------------------");
-//                                for (int[] value : bestSolution) {
-//                                    for (int denim : value) {
-//                                        System.out.print(denim + " ");
-//                                    }
-//                                    System.out.println();
-//                                }
-//                                System.out.println("--------------------------------------");
-//                                System.out.println("--------------(HEURISTIC)-------------");
-//                                System.out.println("--------------------------------------");
-//                                for (int[] value : heuristicTable) {
-//                                    for (int denim : value) {
-//                                        System.out.print(denim + " ");
-//                                    }
-//                                    System.out.println();
-//                                }
-//                                System.out.println("--------------------------------------");
                             }
                             newBoard[temp][col] = 0;
                             newBoard[row][col] = 1;
                         }
-                        //                    break;
-                    }
-                    //                break;
+                    }                    
                 }
             }
 
             ++flag[lowestHeuristic];
-//                if (totalIterations == 5 && lowestHeuristic == 1) {
             if (flag[lowestHeuristic] == 5) {
                 for (int it = 0; it < (globalDimension * globalDimension); it++) {
                     flag[it] = 0;
                 }
-//                if (flag) {
                 totalIterations = 0;
                 lowestHeuristic = 100000;
 
@@ -204,21 +106,9 @@ public class HillClimbingNQueen {
                     }
                 }
 
-//                System.out.println("--------------------------------------");
-//                System.out.println("BOARD re-set");
-//                System.out.println("--------------------------------------");
-//                for (int[] value : board) {
-//                    for (int denim : value) {
-//                        System.out.print(denim + " ");
-//                    }
-//                    System.out.println();
-//                }
-//                System.out.println("--------------------------------------");
-
                 for (int r = 0; r < globalDimension; r++) {
                     for (int col = 0; col < globalDimension; col++) {
                         int random = (int) Math.floor(Math.random() * (globalDimension - 1));
-//                            System.out.println(" ////////////////// " + random);
                         board[random][r] = 1;
                         break;
                     }
@@ -229,17 +119,6 @@ public class HillClimbingNQueen {
                         bestSolution[i][j] = board[i][j];
                     }
                 }
-
-//                System.out.println("--------------------------------------");
-//                System.out.println("BOARD randomized");
-//                System.out.println("--------------------------------------");
-//                for (int[] value : board) {
-//                    for (int denim : value) {
-//                        System.out.print(denim + " ");
-//                    }
-//                    System.out.println();
-//                }
-//                System.out.println("--------------------------------------");
             }
         }
 
@@ -295,6 +174,4 @@ public class HillClimbingNQueen {
         }
         return foundHeuristic;
     }
-
-
 }
